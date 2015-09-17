925860e11eb369b58db3a5574bcc87bc1a60c576
Rand McKinney <rand@strongloop.com>
2015-09-17 14:06:30 -0700

    Update to loopback-component-explorer
    Change loopback-explorer to loopback-component-explorer

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ app.start = function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-explorer')) {
-      var explorerPath = app.get('loopback-explorer').mountPath;
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
